e7b7adbcabd586f9cf91029d20ab50925b57f065
AndreaRispoli97 <Andrea9715@outlook.it>
2025-07-08 16:08:46 +0200

    Refactor Ciclabile class: add comments for clarity on attributes, constructor, and methods

--- src/org/lessons/java/ciclabile/Ciclabile.java
+++ src/org/lessons/java/ciclabile/Ciclabile.java
@@ -1,15 +1,17 @@
 package org.lessons.java.ciclabile;
 
 public class Ciclabile {
-
+    // attributi e costruzione array
     private int[] arrayInt;
     private int currentIndex;
 
+    // costruttore con array
     public Ciclabile(int[] arrayInt) {
         this.arrayInt = arrayInt;
         this.currentIndex = 0;
     }
 
+    // metodo get
     public int getElementoSuccessivo() {
         if (hasAncoraElementi()) {
             return arrayInt[currentIndex++];
@@ -17,6 +19,7 @@ public class Ciclabile {
         return -1;
     }
 
+    // metodo per verificare se ci sono altri elementi da prendere
     public boolean hasAncoraElementi() {
         // return currentIndex < arrayInt.length;
         if (currentIndex >= arrayInt.length) {
