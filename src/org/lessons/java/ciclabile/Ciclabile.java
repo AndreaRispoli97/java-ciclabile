1a6be68838281e406b1c0f9420794ca37a0ac443
AndreaRispoli97 <Andrea9715@outlook.it>
2025-07-08 16:05:44 +0200

    Implement Ciclabile class methods and update Main class for element retrieval

--- src/org/lessons/java/ciclabile/Ciclabile.java
+++ src/org/lessons/java/ciclabile/Ciclabile.java
@@ -2,4 +2,26 @@ package org.lessons.java.ciclabile;
 
 public class Ciclabile {
 
+    private int[] arrayInt;
+    private int currentIndex;
+
+    public Ciclabile(int[] arrayInt) {
+        this.arrayInt = arrayInt;
+        this.currentIndex = 0;
+    }
+
+    public int getElementoSuccessivo() {
+        if (hasAncoraElementi()) {
+            return arrayInt[currentIndex++];
+        }
+        return -1;
+    }
+
+    public boolean hasAncoraElementi() {
+        // return currentIndex < arrayInt.length;
+        if (currentIndex >= arrayInt.length) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -3,5 +3,17 @@ package org.lessons.java.ciclabile;
 public class Main {
     public static void main(String[] args) {
 
+        int[] numeri = { 10, 20, 30, 40, 50, 60 };
+        Ciclabile elenco = new Ciclabile(numeri);
+
+        // System.out.println(elenco.getElementoSuccessivo());
+        // System.out.println(elenco.hasAncoraElementi());
+
+        while (elenco.hasAncoraElementi()) {
+            System.out.println(elenco.getElementoSuccessivo());
+        }
+
+        // System.out.println(elenco.getElementoSuccessivo());
+        // System.out.println(elenco.hasAncoraElementi());
     }
 }
